06b31263a3a74aa77dcaa2a3e905862551f320eb
gerintuanakotta <gerintuanakotta@gmail.com>
2019-07-31 13:20:36 +0700

    halfway

---
+++ models/user.js
@@ -0,0 +1,21 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+
+  class User extends sequelize.Sequelize.Model {}
+
+  User.init ({
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    username: DataTypes.STRING,
+    password: DataTypes.STRING,
+    role: DataTypes.STRING,
+    logged_in: DataTypes.BOOLEAN
+  }, {
+    sequelize, modelName: "User"
+  });
+
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
