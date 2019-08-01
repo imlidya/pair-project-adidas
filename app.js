036a7f7ce29b3ca69c1271d42fef42efd108f63a
gerintuanakotta <gerintuanakotta@gmail.com>
2019-08-01 15:44:12 +0700

    test

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 3333
+const {productRouter} = require ('./routes')
+const router = express.Router()
 // const routes = require('./routes/productRoutes')
 
 app.use(express.json()) // for parsing application/json
@@ -9,6 +11,7 @@ app.use(express.urlencoded({ extended: false })) // for parsing application/x-ww
 app.set('view engine', 'ejs')
 
 // app.get('/', (req, res) => res.render('home'))
-app.use('/', require('./routes/productRoutes'))
+app.use('/', require(productRouter))
+router.get('/', Controller.buyProduct)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,11 +2,10 @@ const express = require('express')
 const router = express.Router()
 const Controller = require('../controllers/productController')
 
-router.get('/product', Controller.listProduct)
-router.get('/', Controller.buyProduct)
-router.get('/product/add', Controller.form)
-router.post('/product/add', Controller.createProduct)
-router.get('/product/login', Controller.login)
-router.get('/product/delete/:id', Controller.delete)
+router.get('/', Controller.listProduct)
+router.get('/add', Controller.form)
+router.post('/add', Controller.createProduct)
+router.get('/login', Controller.login)
+router.get('/delete/:id', Controller.delete)
 
 module.exports = router
